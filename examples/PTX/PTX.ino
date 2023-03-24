ae9a714de396db829361d7a8b2721a11cea55789
Antti <antti.kurkinen@tuni.fi>
2023-03-24 18:54:25 +0200

    Clean PTX sketch

--- examples/PTX/PTX.ino
+++ examples/PTX/PTX.ino
@@ -8,7 +8,7 @@
 #include "average.h"
 
 // If 1, write extra information to serial output
-#define VERBOSE 0
+#define DEBUG 0
 
 // if 1, add 1ms delay between packed transmissions
 // Scanner is more likely to catch those devices which send data
@@ -17,7 +17,7 @@
 
 // Last payload to send on each wlan channel
 // !! MUST HAVE MATCHING VALUE WITH PRX SKETCH
-#define MAX_PAYLOAD 1000
+#define PAYLOAD_LAST 1000
 
 #define WLAN_CHANNELS 13
 
@@ -28,29 +28,25 @@
 // Call NRF24 constructor
 NRF24 nrf (CE_PIN, CSN_PIN, IRQ_PIN);
 
-// Payload to send, will be incremented on every transmission
-uint16_t payload = 1;
-
-// Current wlan channel indicator
-uint8_t wlan_ch = 1;
-
+uint16_t payload = 1; // Payload to send, will be incremented on every transmission
+uint8_t wlan_ch = 1;  // Current wlan channel indicator
 uint8_t data_buffer;  // Buffer for TX payload
 uint8_t plos_cnt_buf; // Packets lost buffer
 uint8_t arc_cnt_buf;  // Auto retransmits buffer
 
-bool TX_DS_flag;  // Data sent irq
-bool MAX_RT_flag; // Max retransmissions irq
+bool TX_DS_flag;  // Data sent successfully irq flag
+bool MAX_RT_flag; // Max retransmissions reached irq flag
 bool RX_DR_flag;  // Dummy
-bool volatile irq_flag;
+bool volatile irq_flag; // ISR irq flag
 
 // Counter for MAX_RT IRQs. Repeated MAX_RT IRQs indicate that
 // PTX is transmitting on different channel than PRX is receiving
 uint8_t MAX_RT_cnt;
 
-// !! wlan_ch == 1 -> index[0]
-int32_t averages [WLAN_CHANNELS];     // Cumulative average of retransmissions count
-int32_t new_samples [WLAN_CHANNELS];  // Retransmit counts of current iteration
-uint32_t tmp_sample;                  // Buffer
+// Note!! wlan_ch == 1 -> index[0]
+int32_t averages [WLAN_CHANNELS];     // Cumulative average of retransmissions
+int32_t new_samples [WLAN_CHANNELS];  // Current iteration retransmissions
+uint32_t tmp_sample;                  // Sample buffer
 uint32_t iteration_cnt;               // Number of elapsed iterations/channel loops
 
 void irqHandler();
@@ -63,8 +59,7 @@ void calcAvg();
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial)
-  {
+  while (!Serial) {
     // Wait for serial to start
   }
 
@@ -76,7 +71,7 @@ void setup() {
   nrf.set_setup_retr(0x02, 0x0F);         // 750us ARD, 15 ARC
   nrf.set_PA_level(PA_LOW);               // -18dBm
   nrf.set_data_rate(DATARATE_MED);        // 1Mbps
-  nrf.set_payload_size(sizeof(payload));
+  nrf.set_payload_size(sizeof(payload));  // Set correct payload size
   nrf.set_wlan_channel(wlan_ch);          // Set RF freq to wlan channel 1
 
   nrf.print_reg();  // Print values of nrf registers
@@ -86,86 +81,73 @@ void setup() {
 } // setup
 
 void loop() {
-  // TODO Description of the loop!!
-  if (irq_flag) 
-  {
+  // Wait for IRQ in loop
+  if (irq_flag) {
+    irq_flag = 0;
     nrf.read_irqs(&MAX_RT_flag, &TX_DS_flag, &RX_DR_flag);
 
-    if (TX_DS_flag) 
-    {
-      #if VERBOSE
-        if (payload % (MAX_PAYLOAD/5) == 0) {
-          Serial.print("Data sent: ");
-          Serial.println(payload);
-        }
-      #endif
+      if (TX_DS_flag) { // Transmission successful
+        TX_DS_flag = 0;
+        MAX_RT_cnt = 0; // MAX_RT counter can be reseted because transmission was successful
 
-      TX_DS_flag = 0;
-      MAX_RT_cnt = 0; // MAX_RT counter can be reseted because transmission was successful
-
-      // Get the amount of retransmissions of last payload
-      nrf.read_observe_tx(&plos_cnt_buf, &arc_cnt_buf);
-      tmp_sample += arc_cnt_buf;      
-
-      if (payload >= MAX_PAYLOAD) 
-      {
-        #if VERBOSE
-          Serial.print("Last payload sent, retransmissions: ");
-          Serial.println(tmp_sample);
+        #if DEBUG
+          if (payload % (PAYLOAD_LAST/5) == 0) {
+            Serial.print("Data sent: ");
+            Serial.println(payload);
+          }
         #endif
-        
-        hopChannel();
-      }
-      else
-      {
-        ++payload;
-      }
-    }
-    else if (MAX_RT_flag) 
-    {
-        MAX_RT_flag = 0;
 
-        if (++MAX_RT_cnt > 20) 
-        {
-          // ACK packet to PTX most likely lost during channel switching and
-          // PTX device is stuck transmitting last payload of the sequence
-          #if VERBOSE
-            Serial.println("Connection lost to PRX device");
+        // Get the amount of retransmissions of last payload
+        nrf.read_observe_tx(&plos_cnt_buf, &arc_cnt_buf);
+        tmp_sample += arc_cnt_buf;      
+
+        if (payload >= PAYLOAD_LAST) {
+          #if DEBUG
+            Serial.print("Last payload sent, retransmissions: ");
+            Serial.println(tmp_sample);
           #endif
-
-          MAX_RT_cnt = 0;
+          
           hopChannel();
-        }
+        } else
+          ++payload;
+        
+      } else if (MAX_RT_flag) { // Transmission failed
+          MAX_RT_flag = 0;
 
-        #if VERBOSE
-        else 
-        {
-          Serial.print("Retrying transmission with payload: ");
-          Serial.println(payload);  
-        }
+          if (++MAX_RT_cnt > 20) {
+            // ACK packet to PTX most likely lost during channel switching and
+            // PTX device is stuck transmitting last payload of the sequence
+            #if DEBUG
+              Serial.println("Connection lost to PRX device");
+            #endif
+
+            MAX_RT_cnt = 0;
+            hopChannel();
+          }
+
+          #if DEBUG
+          else {
+            Serial.print("Retrying transmission with payload: ");
+            Serial.println(payload);  
+          }
+          #endif
+      }
+
+        #if DELAY
+          delay(1);
         #endif
-    }
 
-    irq_flag = 0;
-
-    #if DELAY
-      delay(1);
-    #endif
-
-    nrf.write_tx_payload(&payload);
-    nrf.transmit();
-
+        nrf.write_tx_payload(&payload);
+        nrf.transmit();
   } // irq_flag
 } // loop
 
-void irqHandler() 
-{
+void irqHandler() {
   irq_flag = 1;
 }
 
-void hopChannel() 
-{
-  // Store retransmissions happened on previous channel
+void hopChannel() {
+  // Store retransmission count of previous channel
   // Move to next wlan channel
   new_samples[wlan_ch-1] = tmp_sample;
   tmp_sample = 0;
@@ -176,14 +158,13 @@ void hopChannel()
     calcAvg();
   }
 
-  #if VERBOSE
+  #if DEBUG
     Serial.print("Current channel: ");
     Serial.println(wlan_ch);
   #endif
 }
 
-void calcAvg() 
-{
+void calcAvg() {
   // Calculate new cumulative average and print it
   ++iteration_cnt;
   AVG.calcNewAvg(averages, new_samples, WLAN_CHANNELS, iteration_cnt);
